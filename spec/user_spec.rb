8ed317642870a8603079c42acc68526aab14c988
Shouvik Mukherjee <contact@ishouvik.com>
2018-05-14 15:23:37 +0530

    Write tests for User class

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -8,5 +8,10 @@ describe "User management" do
     it 'should create a new cab object' do
       expect(user).to be_truthy
     end
+
+    it 'should respond to proper methods' do
+      expect(user).to respond_to(:pickup_location)
+      expect(user).to respond_to(:drop_location)
+    end
   end
 end
