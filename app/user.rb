b2902d7636cf68f4dfe7a31703a1fc503ef4218f
Shouvik Mukherjee <contact@ishouvik.com>
2018-05-14 15:29:01 +0530

    Add pickup, drop location values for User class

--- app/user.rb
+++ app/user.rb
@@ -1,5 +1,10 @@
-class User
-  def initialize
+require 'random-location'
 
+class User
+  attr_accessor :pickup_location, :drop_location
+
+  def initialize
+    @pickup_location = RandomLocation.near_by(12.978291, 77.638594, 1000)
+    @drop_location   = RandomLocation.near_by(12.9972469, 77.638594, 1000)
   end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -13,5 +13,10 @@ describe "User management" do
       expect(user).to respond_to(:pickup_location)
       expect(user).to respond_to(:drop_location)
     end
+
+    it 'should respond to proper data types' do
+      expect(user.pickup_location).to be_an_instance_of(Array)
+      expect(user.drop_location).to   be_an_instance_of(Array)
+    end
   end
 end
