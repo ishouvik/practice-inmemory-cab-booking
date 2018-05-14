9daa673e19d14a8fdabb68c600fc0626c7a19c5e
Shouvik Mukherjee <contact@ishouvik.com>
2018-05-14 15:10:09 +0530

    Create a fleet of go and pink cabs on init

--- app/fleet.rb
+++ app/fleet.rb
@@ -1,7 +1,16 @@
 require_relative './cab.rb'
 
 class Fleet
-  def initialize
+  attr_accessor :size, :go_cabs, :pink_cabs
 
+  def initialize(size = 100)
+    @size = size
+    @go_cabs = []
+    @pink_cabs = []
+
+    0.upto (size) do |index|
+      @go_cabs << Cab.new(index)
+      @pink_cabs << Cab.new(index)
+    end
   end
 end
--- spec/fleet_spec.rb
+++ spec/fleet_spec.rb
@@ -5,8 +5,29 @@ describe "Fleet management" do
   let(:fleet) { Fleet.new }
 
   context "fleet" do
-    it 'should respond to proper methods' do
+    it 'should create a new fleet object' do
       expect(fleet).to be_truthy
+    end
+
+    it 'should respond to proper methods' do
+      expect(fleet).to respond_to(:size)
+      expect(fleet).to respond_to(:go_cabs)
+      expect(fleet).to respond_to(:pink_cabs)
+    end
+
+    it 'should respond to proper data types' do
+      expect(fleet.size).to be_an_instance_of(Integer)
+      expect(fleet.size).to be > 0
+      expect(fleet.go_cabs).to be_an_instance_of(Array)
+      expect(fleet.pink_cabs).to be_an_instance_of(Array)
+
+      fleet.go_cabs.each do |cab|
+        expect(cab).to be_an_instance_of(Cab)
+      end
+
+      fleet.pink_cabs.each do |cab|
+        expect(cab).to be_an_instance_of(Cab)
+      end
     end
   end
 end
