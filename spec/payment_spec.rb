5d35126d76a436cfb2d1313e811915fe0ef4bd63
Shouvik Mukherjee <contact@ishouvik.com>
2018-05-14 16:09:17 +0530

    Code refactor

--- spec/payment_spec.rb
+++ spec/payment_spec.rb
@@ -3,6 +3,8 @@ require_relative '../helper/payment'
 
 describe "Payment helper" do
   let(:payment) { Payment.new }
+  let(:distance) { 64.12345 }
+  let(:car_type) { 'pink' }
 
   context "payment" do
     it 'should create a new payment object' do
@@ -15,7 +17,8 @@ describe "Payment helper" do
   end
 
   context "payment amount" do
-    subject { Payment.amount(64.12345, 'pink') }
+    subject { Payment.amount(distance, car_type) }
+
     it 'should return calculated amount' do
       expect(subject).to be_an_instance_of(Float)
     end
