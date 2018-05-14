03d68db3662bb1980003eaa3b237889276db6917
Shouvik Mukherjee <contact@ishouvik.com>
2018-05-14 14:41:51 +0530

    Initialize project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fuber
+
+Book cabs from a fleet of regular and pink cabs
---
+++ boot.rb
@@ -0,0 +1,28 @@
+# Main module
+
+# Initialize
+# Create user
+# Initialize Fleet
+
+puts "PLEASE SELECT CAB TYPE"
+puts "[1] PINK"
+puts "[2] GO"
+cab_type = gets.to_i
+
+if cab_type == 1
+  # Do something for pink
+else
+  # Do something for non-pink
+end
+
+puts "PLEASE SELECT YOUR CAB"
+
+
+puts "Cab No. is arriving at your location"
+sleep 5
+puts "Cab No. has arrived at your location"
+sleep 5
+puts "You cab reached your destination."
+puts "Distance: XXX"
+puts "Payable amount: XXX"
+puts "Cab type: go"
