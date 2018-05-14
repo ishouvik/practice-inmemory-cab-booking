25b4ee1a0f1ee1da3fed9d18f4b798a7f86f0b59
Shouvik Mukherjee <contact@ishouvik.com>
2018-05-14 15:45:54 +0530

    Fix typos

--- boot.rb
+++ boot.rb
@@ -21,24 +21,26 @@ cab_type = gets.to_i
 if cab_type == 1
   cabs           = fleet.pink_cabs
   available_cabs = fleet.available_pink_cabs
-  payable_amount = Payable.amount(travel_distance, 'pink')
+  payable_amount = Payment.amount(travel_distance, 'pink')
 else
   cabs           = fleet.go_cabs
   available_cabs = fleet.available_go_cabs
-  payable_amount = Payable.amount(travel_distance, 'go')
+  payable_amount = Payment.amount(travel_distance, 'go')
 end
 
 puts "PLEASE SELECT YOUR CAB"
 puts available_cabs.map { |cab| cab.number }.join(', ')
 cab_number = gets.to_i
+cabs[cab_number].available = false
 
 puts "Cab No. is arriving at your location"
-sleep 5
+# sleep 5
 
 puts "Cab No. has arrived at your location"
-sleep 5
+# sleep 5
 
 puts "Cab type: #{cab_type}"
 puts "Your cab reached your destination."
-puts "Distance: XXX"
-puts "Payable amount: XXX"
+puts "Distance: #{travel_distance}"
+puts "Payable amount: #{payable_amount} DragonCoin"
+cabs[cab_number].available = true
--- helper/payment.rb
+++ helper/payment.rb
@@ -1,5 +1,5 @@
 class Payment
-  def amount(distance, car_type)
+  def self.amount(distance, car_type)
     dragoncoin = (distance * 2)
     (car_type == 'pink') ? (return (dragoncoin + 5)) : (return dragoncoin)
   end
